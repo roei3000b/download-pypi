549a569e48dbc077e28d4b33f9565c2b8ea93eec
roei <roei3000b@gmail.com>
2018-07-19 00:09:18 +0300

    add upload date to Package

--- Package.py
+++ Package.py
@@ -1,11 +1,11 @@
 class Package:
-    def __init__(self, name, download_url):
+    def __init__(self, name, download_url, upload_date):
         self.name = name
         self.download_url = download_url
+        self.upload_date = upload_date
 
     def download(self, root_directory="."):
         """
         download this package to root_directory in the path of the package as it shown in the download_url
         """
         raise NotImplemented("Not implemented!")
-
