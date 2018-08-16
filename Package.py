749586d03ac0804a94790de99091cb6a8f75ce8a
roei <roei3000b@gmail.com>
2018-08-17 00:19:20 +0300

    implement download method

--- Package.py
+++ Package.py
@@ -1,11 +1,17 @@
+import shutil
+import requests
+
+
 class Package:
     def __init__(self, name, download_url, upload_date):
         self.name = name
         self.download_url = download_url
         self.upload_date = upload_date
 
-    def download(self, root_directory="."):
+    def download(self, dest_path="."):
         """
         download this package to root_directory in the path of the package as it shown in the download_url
         """
-        raise NotImplemented("Not implemented!")
+        with requests.get(self.download_url, stream=True) as response:
+            with open(dest_path, 'wb') as f:
+                shutil.copyfileobj(response.raw, f)
